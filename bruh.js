1f14740d2b3eab54f70390546b129b14a8de9897
Brune <168581255+BruneCoding@users.noreply.github.com>
2025-01-12 21:09:54 +0000

    L

--- bruh.js
+++ bruh.js
@@ -1,29 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleDay = document.querySelector(".toggle-day");
-  const iframe = document.querySelector('.bruhframe');
-  const e = document.querySelector('#d');
+window.location.href = "https://europemc-map-day.pages.dev";
 
-  toggleDay.addEventListener("mouseover", function () {
-    const randomRotation = (Math.random() * 6 - 3).toFixed(2); 
-    toggleDay.style.transform = `scale(1.1) rotate(${randomRotation}deg)`;
-    toggleDay.style.opacity = "0.8";
-  });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const toggleDay = document.querySelector(".toggle-day");
+//   const iframe = document.querySelector('.bruhframe');
+//   const e = document.querySelector('#d');
 
-  toggleDay.addEventListener("mouseout", function () {
-    toggleDay.style.transform = "scale(1)";
-    toggleDay.style.opacity = "1";
-  });
+//   toggleDay.addEventListener("mouseover", function () {
+//     const randomRotation = (Math.random() * 6 - 3).toFixed(2); 
+//     toggleDay.style.transform = `scale(1.1) rotate(${randomRotation}deg)`;
+//     toggleDay.style.opacity = "0.8";
+//   });
 
-  toggleDay.addEventListener("click", function () {
-    if (iframe.src.includes("europemc-map-night.pages.dev")) {
-      iframe.src = "https://europemc-map.pages.dev";
-      e.innerHTML = "Night";
-    } else {
-      iframe.src = "https://europemc-map-night.pages.dev";
-      e.innerHTML = "Day";
-    }
-  });
-});
+//   toggleDay.addEventListener("mouseout", function () {
+//     toggleDay.style.transform = "scale(1)";
+//     toggleDay.style.opacity = "1";
+//   });
+
+//   toggleDay.addEventListener("click", function () {
+//     if (iframe.src.includes("europemc-map-night.pages.dev")) {
+//       iframe.src = "https://europemc-map.pages.dev";
+//       e.innerHTML = "Night";
+//     } else {
+//       iframe.src = "https://europemc-map-night.pages.dev";
+//       e.innerHTML = "Day";
+//     }
+//   });
+// });
 
 
-alert('on Betea Preview')
+// alert('on Betea Preview')

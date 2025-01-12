3f4fe9e6f7a8aad4572af1c7cfe6f7d298e94b93
Brune <168581255+BruneCoding@users.noreply.github.com>
2025-01-12 20:58:47 +0000

    Created bruh

---
+++ bruh.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleDay = document.querySelector(".toggle-day");
+  const iframe = document.querySelector('.bruhframe');
+  const e = document.querySelector('#d');
+
+  toggleDay.addEventListener("mouseover", function () {
+    const randomRotation = (Math.random() * 6 - 3).toFixed(2); 
+    toggleDay.style.transform = `scale(1.1) rotate(${randomRotation}deg)`;
+    toggleDay.style.opacity = "0.8";
+  });
+
+  toggleDay.addEventListener("mouseout", function () {
+    toggleDay.style.transform = "scale(1)";
+    toggleDay.style.opacity = "1";
+       });
+
+  toggleDay.addEventListener("click", function () {    if (iframe.src.includes("europemc-map-night.pages.dev")) {
+      iframe.src = "https://europemc-map.pages.dev";
+      e.innerHTML = "Night";
+    } else {
+      iframe.src = "https://europemc-map-night.pages.dev";
+      e.innerHTML = "Day";
+    }
+  });
+});
+
+
+
